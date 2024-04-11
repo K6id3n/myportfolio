54d64004955eaa1d282f84fadb6addd8dabfb173
K6id3n <kaidengarza203@gmail.com>
2024-04-11 06:58:14 -0700

    ...

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ window.addEventListener('scroll', function () {
     document.getElementById('opText').style.fontSize = (Math.max(18 - window.scrollY / 20)) + 'px';
     document.getElementById('opText').style.opacity = Math.max(0, 1 - window.scrollY / 500);
     let pos = window.scrollY;
-    if (pos >= 344) {
-        pos = 344;
+    let inner = window.innerHeight / 3;
+    if (pos >= inner) {
+        pos = inner;  
+        if (window.scrollY <= (inner) + 150) {
+            window.scrollTo({
+                top: inner,
+                behavior: 'smooth'
+            }); 
+        }
+        
     }
     document.getElementById('fol').style.transform = 'translateY(' + -(pos * 2) + 'px)';
-   
 });
