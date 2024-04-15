4abf5194b2b49c0cb30901b1c741f4de471e47db
K6id3n <kaidengarza203@gmail.com>
2024-04-15 01:39:35 -0700

    wow iphon

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// window.addEventListener('scroll', function () {
-//     document.getElementById('opText').style.fontSize = (Math.max(18 - window.scrollY / 20)) + 'px';
-//     document.getElementById('opText').style.opacity = Math.max(0, 1 - window.scrollY / 500);
-//     document.getElementById('fol').style.marginTop = (100 - (window.scrollY / (window.innerHeight / 100))) + 'dvh';
-//     document.querySelector('.title').style.backgroundPositionY = (window.scrollY * 0.2) + 'px'; 
-// });
+window.addEventListener('scroll', function () {
+    document.getElementById('opText').style.fontSize = (Math.max(18 - window.scrollY / 20)) + 'px';
+    document.getElementById('opText').style.opacity = Math.max(0, 1 - window.scrollY / 500);
+    document.getElementById('fol').style.marginTop = (100 - (window.scrollY / (window.innerHeight / 100))) + 'dvh';
+    document.querySelector('.title').style.backgroundPositionY = (window.scrollY * 0.2) + 'px'; 
+});
 document.addEventListener('mousemove', function(e) {
-    var cursor = document.querySelector('.cursor');
+    var cursor = document.getElementById('cur');
     setTimeout(function() {
         cursor.style.left = (e.pageX - 10) + 'px'; 
         cursor.style.top = (e.pageY - 10) + 'px';  
-    }, 50)
+    }, 200)
 });
