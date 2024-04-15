4010fa7cbce83752d1a6052e370e7b6c865efb12
K6id3n <kaidengarza203@gmail.com>
2024-04-15 01:32:59 -0700

    .0.

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-window.addEventListener('scroll', function () {
-    document.getElementById('opText').style.fontSize = (Math.max(18 - window.scrollY / 20)) + 'px';
-    document.getElementById('opText').style.opacity = Math.max(0, 1 - window.scrollY / 500);
-    let pos = window.scrollY;
-    let inner = window.innerHeight / 3;
-    if (pos >= inner) {
-        pos = inner;
-        if (window.scrollY >= (inner)) {
-            window.scrollTo({
-                top: inner,
-                behavior: 'smooth'
-            });
-        }
-
-    }
-    document.getElementById('fol').style.transform = 'translateY(' + -(pos * 2) + 'px)';
+// window.addEventListener('scroll', function () {
+//     document.getElementById('opText').style.fontSize = (Math.max(18 - window.scrollY / 20)) + 'px';
+//     document.getElementById('opText').style.opacity = Math.max(0, 1 - window.scrollY / 500);
+//     document.getElementById('fol').style.marginTop = (100 - (window.scrollY / (window.innerHeight / 100))) + 'dvh';
+//     document.querySelector('.title').style.backgroundPositionY = (window.scrollY * 0.2) + 'px'; 
+// });
+document.addEventListener('mousemove', function(e) {
+    var cursor = document.querySelector('.cursor');
+    setTimeout(function() {
+        cursor.style.left = (e.pageX - 10) + 'px'; 
+        cursor.style.top = (e.pageY - 10) + 'px';  
+    }, 50)
 });
