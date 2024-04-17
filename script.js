603870855aabbfe3969bfd42d4f6322fdf394532
K6id3n <kaidengarza203@gmail.com>
2024-04-17 06:57:56 -0700

    small changes

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+
+
 window.addEventListener('scroll', function () {
+    if (!((document.documentElement.scrollHeight - (document.documentElement.scrollTop + window.innerHeight)) <= 1)) {
     document.getElementById('opText').style.fontSize = (Math.max(18 - window.scrollY / 20)) + 'px';
     document.getElementById('opText').style.opacity = Math.max(0, 1 - window.scrollY / 500);
     document.getElementById('fol').style.marginTop = (100 - (window.scrollY / (window.innerHeight / 100))) + 'dvh';
-    document.querySelector('.title').style.backgroundPositionY = (window.scrollY * 0.2) + 'px'; 
+    document.querySelector('.title').style.backgroundPositionY = (window.scrollY * (window.innerHeight / 4000)) + 'px';
+    }
 });
-// document.addEventListener('mousemove', function(e) {
-//     var cursor = document.getElementById('cur');
-//     setTimeout(function() {
-//         cursor.style.left = (e.pageX - 10) + 'px'; 
-//         cursor.style.top = (e.pageY - 10) + 'px';  
-//     }, 100)
-// });
+
